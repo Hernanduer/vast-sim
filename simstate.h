e433fefa103f912790b1e9bb804dc20a94979a55
Hernanduer <epasonic@gmail.com>
2024-05-09 17:30:15 -0500

    Small cleanup

--- simstate.h
+++ simstate.h
@@ -24,11 +24,6 @@ struct SimState
     // Vector of all unloading stations in sim
     std::vector<std::shared_ptr<UnloadingStation>> unloadingStations;
 
-    // Queue tracking trucks waiting for an empty unloading station
-    std::queue<std::shared_ptr<Truck>> waitingTrucks;
-    // Queue tracking empty unloading station if there are no waiting trucks
-    std::queue<std::shared_ptr<UnloadingStation>> emptyUnloadingStations;
-
     // Provide a unified place to track how long a tick is worth - could be made configurable using templates
     using TickType = std::chrono::seconds;
 };
